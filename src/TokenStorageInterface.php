c12920929d8d37d72f196bba8526f57a0f5192a4
Chris <chris@clyates.plus.com>
2025-03-21 20:20:20 +0000

    Update interfaces

--- src/TokenStorageInterface.php
+++ src/TokenStorageInterface.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace BeastBytes\Token;
 
-interface TokenStorageInterface extends TokenManagerInterface
+interface TokenStorageInterface
 {
+    public function add(Token $token): bool;
+    public function clear(): void;
+    public function delete(Token $token): bool;
+    public function exists(string $token): bool;
+    public function get(string $token): ?Token;
 }
