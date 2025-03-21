0264efda6b2579033193d8fc26fc1d9146f16c72
Chris <chris@clyates.plus.com>
2025-03-21 21:02:47 +0000

    Create Token objects in TokenManager

--- src/TokenManager.php
+++ src/TokenManager.php
@@ -6,18 +6,39 @@ namespace BeastBytes\Token;
 
 final class TokenManager implements TokenManagerInterface
 {
-    public function __construct(private TokenStorageInterface $storage)
+    public function __construct(
+        private readonly TokenStorageInterface $storage,
+        private readonly TokenFactoryInterface $factory,
+    )
     {
     }
 
-    public function add(Token $token): bool
+    /**
+     * Add a new token to the storage
+     *
+     * @param TokenTypeInterface $tokenType Token type to add
+     * @param string $userId The user ID the token applies to
+     * @throws \DateMalformedIntervalStringException // PHP >= 8.3
+     */
+    public function add(TokenTypeInterface $tokenType, string $userId): bool
     {
+        $token = new Token(
+            $this->factory->create(),
+            $tokenType,
+            $userId
+        );
+
         return $this
             ->storage
             ->add($token)
         ;
     }
 
+    /**
+     * Clear all tokens from the storage
+     *
+     * @return void
+     */
     public function clear(): void
     {
         $this
@@ -26,28 +47,45 @@ final class TokenManager implements TokenManagerInterface
         ;
     }
 
+    /**
+     * Delete a token from the storage
+     *
+     * @param Token $token
+     * @return bool
+     */
+    public function delete(Token $token): bool
+    {
+        return $this
+            ->storage
+            ->delete($token)
+        ;
+    }
+
+    /**
+     * Test if a token exists in the storage
+     *
+     * @param string $token
+     * @return bool
+     */
     public function exists(string $token): bool
     {
         return $this
             ->storage
             ->exists($token)
         ;
-
     }
 
+    /**
+     * Get a token from the storage
+     *
+     * @param string $token
+     * @return Token|null
+     */
     public function get(string $token): ?Token
     {
         return $this
             ->storage
             ->get($token)
-        ;
-    }
-
-    public function delete(Token $token): bool
-    {
-        return $this
-            ->storage
-            ->delete($token)
         ;
     }
 }
